b1e56a8e2331975980af279c9a1f5f6aed50ef06
xanendorf <xanendorf@gmail.com>
2018-09-30 17:00:57 -0500

    Added Detector, Station Download and Parse. Work to be done TimeStamps and extracting Station Data

---
+++ Unsolved/config.py
@@ -0,0 +1,6 @@
+# # Twitter API Keys
+consumer_key = "5ebmV70DmyiO4kbSgOHxH2bpk"
+consumer_secret = "7wAdGh0I1kO6cdPKxTzRV0dLSKoElcdPaLE0wCMI60s03I2akI"
+access_token = "1029794545792417792-VhsSzlMTIpzfrtUVEqwG5L5ox0ek5w"
+access_token_secret = "qt7EWdX6ZRGJtGcHBNNp7DYLbnxHHqscwCar98zte4TLn"
+weather_key = "c3a0d8cd1da13164b43124fa82bffefb"
